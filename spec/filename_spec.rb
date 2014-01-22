d5a5da180c5bb58e0cf4880d077dfc0264f53680
Max Power <kevin.melchert@gmail.com>
2014-01-22 01:04:20 +0100

    remove turn gem

--- spec/filename_spec.rb
+++ spec/filename_spec.rb
@@ -1,10 +1,8 @@
 # -*- encoding: utf-8 -*-
 require 'rubygems'
 require 'bundler/setup'
-
 require 'minitest/autorun'
 require 'minitest/spec'
-require 'turn/autorun'
 
 require 'filename'
 require 'pathname'
